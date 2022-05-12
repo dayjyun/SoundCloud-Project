a7e839ae9b80fcf01270a7c25c1afce75721becd
kevin <kevinbarrios@ymail.com>
2022-05-12 18:09:12 -0500

    Phase 5 complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,28 +2,10 @@ const express = require("express");
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
-
-// Log in
-router.post('/', async (req, res, next) => {
-    const { credential, password } = req.body;
-
-    const user = await User.login({ credential, password });
-
-    if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
-    }
-
-    await setTokenCookie(res, user);
-
-    return res.json({ user });
-  }
-);
 
 // Restore session user
 router.get('/', restoreUser, (req, res) => {
@@ -40,6 +22,33 @@ router.get('/', restoreUser, (req, res) => {
 router.delete('/', (_req, res) => {
     res.clearCookie('token');
     return res.json({ message: 'success' });
+  }
+);
+
+const validateLogin = [
+  check("credential").exists({ checkFalsy: true }).notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password").exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
+// Log in
+router.post('/', validateLogin, async (req, res, next) => {
+    const { credential, password } = req.body;
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
   }
 );
 
