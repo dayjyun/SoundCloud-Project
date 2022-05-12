31909528fdb0b2e4ca47387c2ea1911c95f81851
kevin <kevinbarrios@ymail.com>
2022-05-12 03:54:40 -0500

    completed phase 3 and 4

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { Model, Validator } = require('sequelize');
+const bcrypt = require('bcryptjs')
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -9,7 +10,33 @@ module.exports = (sequelize, DataTypes) => {
       return { id, username, email };
     }
     validatePassword(password) {
-      
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
+    static async login({ credential, password }) {
+      const { Op } = require("sequelize");
+      const user = await User.scope("loginUser").findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential,
+          },
+        },
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope("currentUser").findByPk(user.id);
+      }
+    }
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword,
+      });
+      return await User.scope("currentUser").findByPk(user.id);
     }
     static associate(models) {
       // define association here
---
+++ backend/routes/api/session.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+
+// Log in
+router.post('/', async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
+  }
+);
+
+// Restore session user
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
+
+// Log out <-- Did this work? I still see the Token
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
+module.exports = router;
