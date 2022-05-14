962ae95f093844cbe672f98cb7bb098b32780e05
kevin <kevinbarrios@ymail.com>
2022-05-14 15:08:07 -0500

    testing error

--- backend/app.js
+++ backend/app.js
@@ -43,7 +43,7 @@ app.use(routes);
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
   const err = new Error("The requested resource couldn't be found.");
-  err.title = "Resource Not Found";
+  err.title = "Wrong page";
   err.errors = ["The requested resource couldn't be found."];
   err.status = 404;
   next(err);
