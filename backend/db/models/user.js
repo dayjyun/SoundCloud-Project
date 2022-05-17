0a7e01a86e5dd95f95af4f5473372500aaace6a8
kevin <kevinbarrios@ymail.com>
2022-05-16 23:03:46 -0500

    completed Albums

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -42,76 +42,79 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isAlpha: true,
-        len: [1, 50]
-      }
-    },
-    lastName: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        isAlpha: true,
-        len: [1, 50]
-      }
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [4, 30],
-        isNotEmail(value) {
-          if (Validator.isEmail(value)) {
-            throw new Error('Cannot be an email')
-          }
-        }
-      }
-    },
-    email:{
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 256]
-      }
-    },
-    hashedPassword: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [60, 60]
-      }
-    },
-    imageUrl: {
-      type: DataTypes.VARCHAR
-    },
-    totalSongs: {
-      type: DataTypes.INTEGER
-    },
-    totalAlbums: {
-      type: DataTypes.INTEGER
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-    defaultScope: {
-      attributes: {
-        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
-      }
-    },
-    scopes: {
-      currentUser: {
-        attributes: {
-          exclude: ['hashedPassword']
-        }
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+          len: [1, 50],
+        },
       },
-      loginUser: {
-        attributes: {}
-      }
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isAlpha: true,
+          len: [1, 50],
+        },
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email");
+            }
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+        },
+      },
+      hashedPassword: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
+        },
+      },
+      imageUrl: {
+        type: DataTypes.VARCHAR,
+      },
+      totalSongs: {
+        type: DataTypes.INTEGER,
+      },
+      totalAlbums: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
+        },
+      },
+      scopes: {
+        currentUser: {
+          attributes: {
+            exclude: ["hashedPassword"],
+          },
+        },
+        loginUser: {
+          attributes: {},
+        },
+      },
     }
-  });
+  );
   return User;
 };
