3f143c50da69e03dab67f242e3f45e6aee5767d8
kevin <kevinbarrios@ymail.com>
2022-05-20 15:04:26 -0500

    completed get deatils of an album from an id

--- backend/routes/albums.js
+++ backend/routes/albums.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const { Album } = require('../db/models')
+const { requireAuth, restoreUser } = require("../utils/auth");
+const { Album, User, Song } = require('../db/models')
 
 // Create a Song for an Album with Album Id 351 TRUE (CURRENT USER)
 
@@ -14,7 +15,24 @@ router.get('/albums', async (req, res) => {
 
 
 // Get album details using album ID 596
+router.get('/albums/:albumId', async(req, res) => {
+    const { albumId } = req.params;
+    const album = await Album.findByPk(albumId, {
+        include: [
+            { model: User, as: "Artist", attributes: [ 'id', 'username', 'imageUrl' ] },
+            { model: Song }
+        ]
+    })
 
+    if (!album) {
+        res.json({
+          message: "Album couldn't be found",
+          statusCode: 404,
+        });
+    }
+
+    res.json(album)
+})
 
 // Create an album 655 TRUE
 
