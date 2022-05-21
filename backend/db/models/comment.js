af94d42cdae09fcc0fd965cfdf24099c36fcffd0
kevin <kevinbarrios@ymail.com>
2022-05-21 12:20:39 -0500

    delete song stuff

--- backend/db/models/comment.js
+++ backend/db/models/comment.js
@@ -12,11 +12,9 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Comment.belongsTo(models.User, {
         foreignKey: "userId",
-        onDelete: "CASCADE",
       }),
         Comment.belongsTo(models.Song, {
           foreignKey: "songId",
-          onDelete: "CASCADE",
         });
     }
   }
