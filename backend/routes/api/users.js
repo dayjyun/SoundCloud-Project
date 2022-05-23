423056108139cfbe18ba9a8c950bc3250dbcbbfb
kevin <kevinbarrios@ymail.com>
2022-05-23 00:27:59 -0500

    fixed a few snytax occurences

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -16,7 +16,10 @@ const validateSignup = [
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
     .withMessage("Please provide a username with at least 4 characters."),
-  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("username")
+    .not()
+    .isEmail()
+    .withMessage("Username cannot be an email."),
   check("password")
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
@@ -29,10 +32,16 @@ router.post('/signup', validateSignup, async (req, res) => {
     const { firstName, lastName, email, password, username } = req.body;
     // const token = await setTokenCookie(res, user)
     const checkEmail = await User.findOne({ where: { email } })
+    const checkUsername = await User.findOne({ where: { username } })
 
     if(checkEmail) {
       let error = new Error('E-mail already exists');
       error.status = 403
+      throw error;
+    }
+    if (checkUsername) {
+      let error = new Error("Username already exists");
+      error.status = 403;
       throw error;
     }
 
@@ -40,7 +49,7 @@ router.post('/signup', validateSignup, async (req, res) => {
 
     const token = await setTokenCookie(res, user);
 
-    return res.json({ user, token });
+    return res.json({ ...user.toSafeObject(), token });
   }
 );
 
