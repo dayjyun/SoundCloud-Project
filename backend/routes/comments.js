63a41a07da2f7be0bcf4c87128bc8bd4e9edc1bb
kevin <kevinbarrios@ymail.com>
2022-05-23 22:37:01 -0500

    completed delete comment

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -10,6 +10,29 @@ router.get('/commentsTest', (req, res) => {
 
 
 // Delete a comment 991 TRUE (CURRENT USER)
+router.delete("/comments/:commentId", requireAuth, async (req, res) => {
+  const { user } = req;
+  const { commentId } = req.params;
 
+  const comment = await Comment.findByPk(commentId);
+
+  if (comment) {
+    if (comment.userId === user.id) {
+      comment.destroy();
+      res.json({
+        message: "Successfully deleted",
+        statusCode: 200,
+      });
+    } else {
+      const error = new Error("Unauthorized");
+      error.statusCode = 401;
+      throw error;
+    }
+  } else {
+    const error = new Error("Comment not found");
+    error.status = 404;
+    throw error;
+  }
+});
 
 module.exports = router;
