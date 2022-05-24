377c127f7da502782ac67eb71804396af213b7ad
kevin <kevinbarrios@ymail.com>
2022-05-24 15:39:15 -0500

    completed get all songs of an artist by Id

--- backend/routes/artists.js
+++ backend/routes/artists.js
@@ -4,14 +4,23 @@ const { check } = require("express-validator");
 
 const { requireAuth, restoreUser } = require("../utils/auth");
 const { handleValidationErrors } = require("../utils/validation");
-const { User, Song, Album } = require("../db/models");
+const { User, Song, Album, } = require("../db/models");
 
 // Get details of an artist/user by Artist ID 1028
 
 // Get all Songs of an artist by Id 1132
-router.get('artists/:artistId/songs', async(req, res) => {
+router.get('/artists/:artistId/songs', async(req, res) => {
   const { artistId } = req.params;
-  const Songs = await Song.findAll()
+  const artist = await User.findByPk(artistId);
+
+  if(artist) {
+    const Songs = await Song.findAll({ where: { userId: artistId } })
+    res.json({ Songs });
+  } else {
+    const error = new Error("Song not found");
+    error.status = 404;
+    throw error;
+  }
 })
 
 // Get all albums of an artist by Id 1182
