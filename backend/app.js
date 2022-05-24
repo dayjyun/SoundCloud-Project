0691a7bf17a51eca76661f16130a8a527f0495a8
kevin <kevinbarrios@ymail.com>
2022-05-23 19:33:16 -0500

    fixed error messages

--- backend/app.js
+++ backend/app.js
@@ -65,7 +65,6 @@ app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
   res.json({
-    // title: err.title || 'Server Error',
     message: err.message,
     statusCode: res.statusCode,
     errors: err.errors,
