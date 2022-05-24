6ffeb8fb4a83ce1eda5d8148150881163e5fa816
kevin <kevinbarrios@ymail.com>
2022-05-23 19:24:18 -0500

    fixed validation.js error title

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -8,10 +8,9 @@ const handleValidationErrors = (req, _res, next) => {
   if (!validationErrors.isEmpty()) {
     const errors = validationErrors.array().map((error) => `${error.msg}`);
 
-    const err = Error('Bad request.');
+    const err = Error('Validation Error');
     err.errors = errors;
     err.status = 400;
-    err.title = 'Bad request.';
     next(err);
   }
   next();
