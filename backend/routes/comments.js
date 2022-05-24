806bc8852e793aac91f813d73884cef322077252
kevin <kevinbarrios@ymail.com>
2022-05-23 22:18:30 -0500

    completed edit comment

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require("express-validator");
+const { requireAuth, restoreUser } = require("../utils/auth");
+const { handleValidationErrors } = require("../utils/validation");
+
+const { Comment } = require("../db/models");
+
+// Validations
+const editComment = [
+    check("body")
+        .exists({ checkFalsy: true })
+        .withMessage("Comment required"),
+    handleValidationErrors
+]
 
 // test
 router.get('/commentsTest', (req, res) => {
@@ -7,6 +20,30 @@ router.get('/commentsTest', (req, res) => {
 });
 
 // Edit a comment 926 TRUE (CURRENT USER)
+router.put('/comments/:commentId', requireAuth, editComment, async(req, res) => {
+    const { user } = req;
+    const { commentId } = req.params;
+    const { body } = req.body;
+
+    const comment = await Comment.findByPk(commentId)
+
+    if(comment) {
+        if(comment.userId === user.id) {
+            await comment.update({
+                body
+            })
+            res.json(comment);
+        } else {
+            const error = new Error("Unauthorized");
+            error.status = 401;
+            throw error;
+        }
+    } else {
+        const error = new Error("Comment not found");
+        error.status = 404;
+        throw error;
+    }
+})
 
 
 // Delete a comment 991 TRUE (CURRENT USER)
