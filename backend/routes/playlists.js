a869c6e35a5e5c1a5662c532a647305a3ab128e2
kevin <kevinbarrios@ymail.com>
2022-05-24 18:39:35 -0500

    created add song to playlist

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -1,32 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
-// test
-router.get('/playlistsTest', (req, res) => {
-    res.json('Playlists works')
-})
+const { requireAuth, restoreUser } = require("../utils/auth");
+const { handleValidationErrors } = require("../utils/validation");
+const { Playlist, Song } = require("../db/models");
 
 // GET
 
 // Get details of a playlist using Playlist ID
 
-
 // POST
 
 // Create a playlist 1203 TRUE
 
-
 // Add a song to a playlist using Playlist ID 1255 TRUE (CURRENT USER)
+router.post("/playlists/:playlistId", requireAuth, async(req, res) => {
+    const { playlistId } = req.params;
+    const { user } = req;
+    const { songId } = req.params;
 
+    const playlist = await Playlist.findByPk(playlistId);
+    const song = await Song.findByPk(songId);
+
+    if(playlist) {
+        if(song) {
+
+        } else {
+            const error = new Error("Song not found");
+            error.status = 404;
+            throw error;
+        }
+    } else {
+        const error = new Error("Playlist not found");
+        error.status = 404;
+        throw error;
+    }
+});
 
 // PUT
 
 // Edit a playlist 1367 TRUE (CURRENT USER)
 
-
 // DELETE
 
 // Delete a playlist 1433 TRUE (CURRENT USER)
-
 
 module.exports = router;
