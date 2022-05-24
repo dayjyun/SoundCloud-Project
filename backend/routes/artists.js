0af24040b1f567ef3b1b90d69b47a574abd41b4f
kevin <kevinbarrios@ymail.com>
2022-05-23 23:31:52 -0500

    started get Songs by Artist Id

--- backend/routes/artists.js
+++ backend/routes/artists.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
-// test
-router.get("/artistsTest", (req, res) => {
-  return res.json("Artists works!");
-});
+const { requireAuth, restoreUser } = require("../utils/auth");
+const { handleValidationErrors } = require("../utils/validation");
+const { User, Song, Album } = require("../db/models");
 
 // Get details of an artist/user by Artist ID 1028
 
-
-// Get all songs of an artist by Id 1090
-
-
 // Get all Songs of an artist by Id 1132
-
+router.get('artists/:artistId/songs', async(req, res) => {
+  const { artistId } = req.params;
+  const Songs = await
+})
 
 // Get all albums of an artist by Id 1182
 
-
 // Get all Playlists of an artist by Id 1230
-
 
 module.exports = router;
