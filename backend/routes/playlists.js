9e6b08bcc5a7a717e61558757e38dac844db9fb9
kevin <kevinbarrios@ymail.com>
2022-05-24 19:09:45 -0500

    completed add song to playlist

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -4,7 +4,8 @@ const { check } = require("express-validator");
 
 const { requireAuth, restoreUser } = require("../utils/auth");
 const { handleValidationErrors } = require("../utils/validation");
-const { Playlist, Song } = require("../db/models");
+const { Playlist, Song, PlaylistSong } = require("../db/models");
+const { errorMonitor } = require("pg/lib/query");
 
 // GET
 
@@ -15,30 +16,41 @@ const { Playlist, Song } = require("../db/models");
 // Create a playlist 1203 TRUE
 
 // Add a song to a playlist using Playlist ID 1255 TRUE (CURRENT USER)
-router.post("/playlists/:playlistId", requireAuth, async(req, res) => {
-    const { playlistId } = req.params;
-    const { user } = req;
-    const { songId } = req.params;
+router.post("/playlists/:playlistId", requireAuth, async (req, res) => {
+  const { playlistId } = req.params;
+  const { user } = req;
+  const { songId } = req.body;
 
-    const playlist = await Playlist.findByPk(playlistId);
-    const song = await Song.findByPk(songId);
+  const playlist = await Playlist.findByPk(playlistId);
+  const song = await Song.findByPk(songId);
 
-    if(playlist.userId === user.id) {
-        if(playlist) {
-            if(song) {
-                const updatePlaylist = await Playlist.create({ song })
-                res.json(updatePlaylist)
-            } else {
-                const error = new Error("Song not found");
-                error.status = 404;
-                throw error;
-            }
-        } else {
-            const error = new Error("Playlist not found");
-            error.status = 404;
-            throw error;
-        }
+  if (playlist) {
+    if (song) {
+      if (playlist.userId === user.id) {
+        const updatePlaylistSong = await PlaylistSong.create({
+          playlistId,
+          songId,
+        });
+        const playlistSong = await PlaylistSong.findOne({
+          where: { playlistId, songId },
+          attributes: ["id", "playlistId", "songId"],
+        });
+        res.json(playlistSong);
+      } else {
+          const error = new Error("Unauthorized");
+          error.status = 404;
+          throw error;
+      }
+    } else {
+      const error = new Error("Song not found");
+      error.status = 404;
+      throw error;
     }
+  } else {
+    const error = new Error("Playlist not found");
+    error.status = 404;
+    throw error;
+  }
 });
 
 // PUT
