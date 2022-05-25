3603fbd9a536a89fa138152530bc030e6f32d0e3
kevin <kevinbarrios@ymail.com>
2022-05-25 15:15:15 -0500

    small changes

--- backend/db/seeders/20220517232947-demo-album.js
+++ backend/db/seeders/20220517232947-demo-album.js
@@ -2,33 +2,44 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-   await queryInterface.bulkInsert('Albums', [
-     { // 1
-       title: 'Demo Album',
-       description: 'My First Demo Album',
-       userId: 1
+   await queryInterface.bulkInsert("Albums", [
+     {
+       // 1
+       title: "Demo Album",
+       description: "My First Demo Album",
+       userId: 1,
+       imageUrl: "asdf",
      },
-     { // 2
-      title: "Ready Player 2",
-      description: 'Dedicated to Luigi Players in Mario Games',
-      userId: 2
+     {
+       // 2
+       title: "Ready Player 2",
+       description: "Dedicated to Luigi Players in Mario Games",
+       userId: 2,
+       imageUrl: "asdf",
      },
-     { // 3
-      title: "Third Time Is The Charm",
-      description: 'For my 3 fans',
-      userId: 3
+     {
+       // 3
+       title: "Third Time Is The Charm",
+       description: "For my 3 fans",
+       userId: 3,
+       imageUrl: "asdf",
      },
-     { // 4
-      title: 'Carnavas',
-      description: 'Contains songs like Rusted Wheel and Dream At Tempo 119 ',
-      userId: 4
+     {
+       // 4
+       title: "Carnavas",
+       description: "Contains songs like Rusted Wheel and Dream At Tempo 119 ",
+       userId: 4,
+       imageUrl: "asdf",
      },
-     { // 5
-      title: "Swoon",
-      description: "Contains songs like Panic Switch and Growing Old Is Getting Old",
-      userId: 4
-     }
-   ])
+     {
+       // 5
+       title: "Swoon",
+       description:
+         "Contains songs like Panic Switch and Growing Old Is Getting Old",
+       userId: 4,
+       imageUrl: "asdf",
+     },
+   ]);
   },
 
   async down (queryInterface, Sequelize) {
