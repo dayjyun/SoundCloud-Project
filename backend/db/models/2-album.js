cb89d5c3f4434773d753f235cba48e0e60b23907
kevin <kevinbarrios@ymail.com>
2022-05-26 18:17:25 -0500

    changes

--- backend/db/models/2-album.js
+++ backend/db/models/2-album.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Album extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Album.belongsTo(models.User, { foreignKey: 'userId', as:  "Artist" }),
       Album.hasMany(models.Song, { foreignKey: 'albumId' })
