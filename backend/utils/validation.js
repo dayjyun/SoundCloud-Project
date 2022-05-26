d940ae42aea0e768b41c638ca911faa935f7fa19
kevin <kevinbarrios@ymail.com>
2022-05-25 19:47:26 -0500

    changed wording in validation js to username or email

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -50,7 +50,7 @@ const validateLogin = [
   check("credential")
     .exists({ checkFalsy: true })
     .notEmpty()
-    .withMessage("Please provide a valid email or username."),
+    .withMessage("Please provide a valid username or email."),
   check("password")
     .exists({ checkFalsy: true })
     .withMessage("Please provide a password."),
