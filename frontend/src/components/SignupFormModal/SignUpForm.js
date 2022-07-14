86b0190122e343dac9d42b2c33342f6a0307abc6
kevin <kevinbarrios@ymail.com>
2022-07-14 14:03:54 -0500

    current progress

--- frontend/src/components/SignupFormModal/SignUpForm.js
+++ frontend/src/components/SignupFormModal/SignUpForm.js
@@ -15,7 +15,7 @@ function SignupFormPage() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return <Redirect to="/" />;
+  if (sessionUser) return <Redirect to="/me" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
