e4972c5dea43e638d062dcc0a542f0b929bd3f0f
kevin <kevinbarrios@ymail.com>
2022-07-14 13:01:15 -0500

    current progress css

--- frontend/src/components/CurrentUser/Edit/EditSongForm.jsx
+++ frontend/src/components/CurrentUser/Edit/EditSongForm.jsx
@@ -1,78 +1,87 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from 'react-redux'
-import { useHistory, useParams } from 'react-router-dom'
-import * as actions from '../../../store/songReducer'
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import * as actions from "../../../store/songReducer";
+import "./EditSongForm.css";
 
 export default function EditSongForm({ setShowSongEdit }) {
-  const { songId } = useParams()
-  const dispatch = useDispatch()
-  const history = useHistory()
+  const { songId } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  const user = useSelector(state => state.session.user);
-  const userId = user.id
-  const song = useSelector((state) => state.songs[`${songId}`])
+  const user = useSelector((state) => state.session.user);
+  const userId = user.id;
+  const song = useSelector((state) => state.songs[`${songId}`]);
 
-  const [validationErrors, setValidationErrors] = useState([])
-  const [title, setTitle] = useState(song.title)
-  const [description, setDescription] = useState(song.description)
-  const [previewImage, setPreviewImage] = useState(song.previewImage)
-  const [url, setUrl] = useState(song.url)
+  const [validationErrors, setValidationErrors] = useState([]);
+  const [title, setTitle] = useState(song.title);
+  const [description, setDescription] = useState(song.description);
+  const [previewImage, setPreviewImage] = useState(song.previewImage);
+  const [url, setUrl] = useState(song.url);
 
-  const handleSongFormSubmit = async(e) => {
-    e.preventDefault()
+  const handleSongFormSubmit = async (e) => {
+    e.preventDefault();
 
-    setValidationErrors([])
+    setValidationErrors([]);
 
-    await dispatch(actions.editSong({
-      id: songId,
-      title,
-      description,
-      imageUrl: previewImage,
-      url,
-      userId
-    }))
-    .then(() => {
-    setShowSongEdit(false)
-    history.push(`/songs/${songId}`)
-  })
-  .catch(async res => {
-    const err = await res.json()
-    if(err) {
-      setValidationErrors(err.validationErrors)
-    }})};
+    await dispatch(
+      actions.editSong({
+        id: songId,
+        title,
+        description,
+        imageUrl: previewImage,
+        url,
+        userId,
+      })
+    )
+      .then(() => {
+        setShowSongEdit(false);
+        history.push(`/songs/${songId}`);
+      })
+      .catch(async (res) => {
+        const err = await res.json();
+        if (err) {
+          setValidationErrors(err.validationErrors);
+        }
+      });
+  };
 
   return (
-    <div className="edit-form">
-      <h1>Edit Your Song</h1>
+    <div className="edit-form-container">
+      <div className="edit-song-text">
+        <p>Edit Your Song</p>
+      </div>
       <form onSubmit={handleSongFormSubmit}>
         <ul>
           {Object.values(validationErrors).map((error) => (
             <li key={error}>{error}</li>
           ))}
         </ul>
-        <div className="input-wrapper">
-          <label htmlFor="title">Title</label>
-          <input
-            className="edit-input"
-            type="text"
-            id="title"
-            name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <div className="input-wrapper">
-            <label htmlFor='description'>Description</label>
+        <div className="song-input">
+          <div className="enter">
+            <label htmlFor="title">Title</label>
             <input
               className="edit-input"
-              type='text'
-              id='description'
-              name='description'
+              type="text"
+              id="title"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="enter">
+            <label htmlFor="description">Description</label>
+            <input
+              className="edit-input"
+              type="text"
+              id="description"
+              name="description"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
           </div>
-          <div className="input-wrapper">
-            <label htmlFor="previewImage">Preview Image</label>
+          <div className="enter">
+            <label htmlFor="previewImage">Image</label>
             <input
               className="edit-input"
               type="text"
@@ -82,8 +91,8 @@ export default function EditSongForm({ setShowSongEdit }) {
               onChange={(e) => setPreviewImage(e.target.value)}
             />
           </div>
-          <div className="input-wrapper">
-            <label htmlFor="url">Song URL</label>
+          <div className="enter">
+            <label htmlFor="url">Media</label>
             <input
               className="edit-input"
               type="text"
@@ -93,8 +102,8 @@ export default function EditSongForm({ setShowSongEdit }) {
               onChange={(e) => setUrl(e.target.value)}
             />
           </div>
-          <div>
-            <button className="edit-button" type="submit">
+          <div className="save-button-edit-song">
+            <button className="save-button-song" type="submit">
               Save
             </button>
           </div>
