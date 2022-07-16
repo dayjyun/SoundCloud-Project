df3d81e31c13e159f8327ccd611ee9b35ab43feb
kevin <kevinbarrios@ymail.com>
2022-07-16 11:32:43 -0500

    current progress

---
+++ frontend/src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,19 @@
+import "./ErrorPage.css";
+
+export default function ErrorPage() {
+  return (
+    <>
+    <div className="error-text">
+      <div className='err text-one'>
+        <h1>Do you hear that?</h1>
+      </div>
+      <div className='err text-two'>
+        <h1>Echoes of silence...</h1>
+      </div>
+    </div>
+    <div>
+      <button>Home</button>
+      </div>
+    </>
+  );
+}
