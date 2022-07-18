f2a89a6a991a9e8dc6cdfa927747a2ed6ab1dc12
kevin <kevinbarrios@ymail.com>
2022-07-17 23:01:16 -0500

    completed album details css

--- frontend/src/components/Navigation/SoundCloudText/SoundCloudText.js
+++ frontend/src/components/Navigation/SoundCloudText/SoundCloudText.js
@@ -1,5 +1,16 @@
 import "./SoundCloudText.css";
 
 export default function SoundCloudText() {
-  return <div className="soundCloudText">SoundCloud</div>;
+  return (
+    <>
+      <div className="soundcloud-image-box" >
+        <img
+          className="soundcloud-logo-image"
+          src="https://soundcloudmisc.s3.us-east-2.amazonaws.com/soundcloud_logo_orange.png"
+        />
+      </div>
+    </>
+  );
+
+  // return <div className="soundCloudText">SoundCloud</div>;
 }
